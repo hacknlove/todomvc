31f84f452a80173914c6d1e326cbc4e370f38ddf
hacknlove <antonio@hacknlove.org>
2024-02-04 18:27:41 +0100

    crear componente para la lista de tareas

--- examples/php-mysql/www/lib/views/main.php
+++ examples/php-mysql/www/lib/views/main.php
@@ -4,25 +4,20 @@
   <meta name="description" content="A TodoMVC written with JavaScript es5 features." />
   <meta name="viewport" content="width=device-width,initial-scale=1" />
   <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-  <title>TodoMVC: JavaScript Es6 Webpack</title>
-  <script defer="defer" src="app.bundle.js"></script>
-  <link href="app.css" rel="stylesheet">
+  <title>TodoMVC: PHP - MySQL</title>
+ <link href="app.css" rel="stylesheet">
   <body>
     <section class="todoapp">
       <header class="header">
-        <h1>todos</h1><input class="new-todo" placeholder="What needs to be done?" autofocus />
+        <h1>todos</h1><form action="" method="POST">
+          <input class="new-todo" name="newTask" placeholder="What needs to be done?" autofocus />
+        </form>
       </header>
-      <main class="main">
-        <div class="toggle-all-container"><input class="toggle-all" type="checkbox" /><label class="toggle-all-label" for="toggle-all">Mark all as complete</label></div>
-        <ul class="todo-list"></ul>
-      </main>
-      <footer class="footer"><span class="todo-count"></span>
-        <ul class="filters">
-          <li><a href="/" <?= $type === 'all' ? 'class="selected"' : '' ?>>All</a></li>
-          <li><a href="/active" <?= $type == 'active' ? 'class="selected"' : '' ?>>Active</a></li>
-          <li><a href="/completed" <?= $type == 'completed' ? 'class="selected"' : '' ?>>Completed</a></li>
-        </ul><button class="clear-completed">Clear completed</button>
-      </footer>
+      <?php 
+        if (isset($tasks[0])) {
+          include './lib/views/tasks.php';
+        }
+      ?>
     </section>
     <footer class="info">
       <p>Double-click to edit a todo</p>
---
+++ examples/php-mysql/www/lib/views/tasks.php
@@ -0,0 +1,24 @@
+<main class="main">
+  <div class="toggle-all-container"><input class="toggle-all" type="checkbox" /><label class="toggle-all-label" for="toggle-all">Mark all as complete</label></div>
+  <ul class="todo-list">
+    <?php foreach ($tasks as $task) { ?>
+      <li class="<?= $task['completed'] ? 'completed' : '' ?>">
+        <form action="" method="post">
+          <input type="hidden" name="id" value="<?= $task['id'] ?>" />
+          <button class="view" name="toogle" value="<?= $task['completed'] ? 'false' : 'true' ?>">
+              <input class="toggle" type="checkbox" <?= $task['completed'] ? 'checked' : '' ?>>
+              <label><?php echo $task['title']; ?></label>
+              <button class="destroy"></button>
+          </button>
+        </form>
+      </li>
+    <?php } ?>
+  </ul>
+</main>
+<footer class="footer"><span class="todo-count"></span>
+  <ul class="filters">
+    <li><a href="/" <?= $type === 'all' ? 'class="selected"' : '' ?>>All</a></li>
+    <li><a href="/active" <?= $type == 'active' ? 'class="selected"' : '' ?>>Active</a></li>
+    <li><a href="/completed" <?= $type == 'completed' ? 'class="selected"' : '' ?>>Completed</a></li>
+  </ul><button class="clear-completed">Clear completed</button>
+</footer>
